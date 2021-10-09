6d7f845d490a60e890c9f3255ceb9af4aa3a762c
Amine Kader <aminemarckader@gmail.com>
2021-10-09 17:38:59 +0100

    Update HardwareTypes.jsx

--- src/Components/Dashboard/HardwareTypes.jsx
+++ src/Components/Dashboard/HardwareTypes.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { VictoryPie, VictoryTheme } from "victory";
+import { VictoryPie, VictoryTheme, VictoryChart } from "victory";
 
 export default function HardwareTypes() {
   return (
+
+    <VictoryChart
+    // adding the material theme provided with Victor
+    width={600} 
+    height={400}
+  >
     <VictoryPie
       colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
       theme={VictoryTheme.material}
@@ -16,5 +22,7 @@ export default function HardwareTypes() {
       ]}
       labels={({ datum }) => `${datum.x}: ${datum.y}`}
     />
+  </VictoryChart>
+    
   );
 }
